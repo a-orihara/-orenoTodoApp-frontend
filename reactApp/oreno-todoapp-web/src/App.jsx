43c05496f014ea10043e9b8c21bd37ec3ae44384
a-orihara <ko.ty.es.ty.mk@gmail.com>
2022-08-17 16:17:06 +0900

    Downgrade to react17 and Hot reload functionality added

--- reactApp/oreno-todoapp-web/src/App.jsx
+++ reactApp/oreno-todoapp-web/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>momo募集中yokokokoo</p>
         <a
           className="App-link"
           href="https://reactjs.org"
