836c5cbc1f365698bf1ded05284ccca719873437
a-orihara <ko.ty.es.ty.mk@gmail.com>
2022-08-18 11:52:23 +0900

    deletebutton実装

--- reactApp/oreno-todoapp-web/src/App.jsx
+++ reactApp/oreno-todoapp-web/src/App.jsx
@@ -3,32 +3,77 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function App() {
-  const [ todos, setTodos ] = useState([]);
 
-  useEffect(() => {
+  const [ todoLists, setTodoList ] = useState([]);
+
+  const initialTodos = {
+    id: null,
+    name: '',
+    is_completed: false
+  }
+
+  const [ fetchedTodos, setFetchedTodos ] = useState({initialTodos});
+
+
+
+  const getAxios = () => {
     axios.get('http://localhost:3002/api/v1/todos')
-      .then(resp => {
+      .then(res => {
       // console.log(resp.data)
-      setTodos(resp.data);
+      setTodoList(res.data);
       })
       .catch(e => {
       console.log(e);
       })
-  }, [])
+  }
 
-  const createButton = (req, res) => {};
-  const deleteButton = (req, res) => {};
+  useEffect(() => {
+    getAxios()
+    // axios.get('http://localhost:3002/api/v1/todos')
+    //   .then(res => {
+    //   // console.log(resp.data)
+    //   setTodoList(res.data);
+    //   })
+    //   .catch(e => {
+    //   console.log(e);
+    //   })
+  },[])
 
+  // const createButton = (req, res) => {
+  //   axios.post('http://localhost:3002/api/v1/todos', fetchedTodos)
+  //     .then( res => {
+  //       setFetchedTodos(res.data)
+  //     })
+  //     .catch(e => {
+  //       console.log(e)
+  //     })
+  // };
+
+  const deleteButton = (todoList) => {
+    const sure = window.confirm('Are you sure?');
+    if (sure) {
+      console.log(todoLists.id)
+      axios.delete(`http://localhost:3002/api/v1/todos/${todoList.id}`)
+      .then( () => {
+        // console.log(res.data)
+        getAxios()
+      })
+      .catch(e => {
+        console.log(e)
+      })
+    } 
+  };
 
 
   return (
     <div className="App">
         <p>momo募集中yokokokoo</p>
-        { todos.map((todo, index) => {
+        { todoLists.map((todoList, id) => {
           return(
-            <div key={index}>
-              <p>{todo.name}</p>
-              <p>{todo.index}</p>
+            <div key={id}>
+              <p>{todoList.name}</p>
+              <p>{todoList.id}</p>
+              <button onClick={()=>deleteButton(todoList)}>消すよ</button>
             </div>
           )
         })}
